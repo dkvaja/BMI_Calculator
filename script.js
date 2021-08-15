62610f3e219d5162428b377a5ed3751bc22b67f3
dk_vaja <vaja.dushyant11@gmail.com>
2021-08-15 20:14:42 +0530

    fix bug

--- script.js
+++ script.js
@@ -12,19 +12,20 @@ const calculateBmi = (event) => {
     const weight = document.getElementById('weight');
 
     if (height.value == 0 || weight.value == 0) alert('Please Enter valid Values!');
+    else {
+        const heightInMeter = height.value / 100;
+        const BMI = (weight.value / (heightInMeter * heightInMeter)).toFixed(3);
 
-    const heightInMeter = height.value / 100;
-    const BMI = (weight.value / (heightInMeter * heightInMeter)).toFixed(3);
+        if (BMI < 18.5) setter('Underweight', BMI, '#01b0f1');
+        else if (BMI >= 18.5 && BMI <= 24.9) setter('Normal Weight', BMI, '#91d14f');
+        else if (BMI > 25 && BMI <= 29.9) setter('Overweight', BMI, '#feff05');
+        else if (BMI > 30 && BMI <= 34.9) setter('Obesity Level I', BMI, "#f99301");
+        else if (BMI > 35 && BMI <= 39.9) setter('Obesity Level II', BMI, '#ff6767');
+        else if (BMI >= 40) setter('Obesity Level III', BMI, 'red');
 
-    if (BMI < 18.5) setter('Underweight', BMI, '#01b0f1');
-    else if (BMI >= 18.5 && BMI <= 24.9) setter('Normal Weight', BMI, '#91d14f');
-    else if (BMI > 25 && BMI <= 29.9) setter('Overweight', BMI, '#feff05');
-    else if (BMI > 30 && BMI <= 34.9) setter('Obesity Level I', BMI, "#f99301");
-    else if (BMI > 35 && BMI <= 39.9) setter('Obesity Level II', BMI, '#ff6767');
-    else if (BMI >= 40) setter('Obesity Level III', BMI, 'red');
-
-    // clean up code
-    height.value = weight.value = '';
+        // clean up code
+        height.value = weight.value = '';
+    }
 }
 
 
