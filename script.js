da859ec78a8b431a0db9e18d542e3228f786ef16
dk_vaja <vaja.dushyant11@gmail.com>
2021-08-21 12:17:29 +0530

    change footer text and fix bug

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const setter = (Text, bmi, colorCode) => {
     document.getElementById('result').style.backgroundColor = colorCode;
-    document.getElementById('result_Text').innerText = `You are in ${Text}`;
+    document.getElementById('result_Text').innerText = ` - ${Text}`;
     document.getElementById('bmi_num').innerHTML = `${bmi} &nbsp;`;
 }
 const calculateBmi = (event) => {
@@ -19,9 +19,9 @@ const calculateBmi = (event) => {
         if (BMI < 18.5) setter('Underweight', BMI, '#01b0f1');
         else if (BMI >= 18.5 && BMI <= 24.9) setter('Normal Weight', BMI, '#91d14f');
         else if (BMI > 25 && BMI <= 29.9) setter('Overweight', BMI, '#feff05');
-        else if (BMI > 30 && BMI <= 34.9) setter('Obesity Level I', BMI, "#f99301");
-        else if (BMI > 35 && BMI <= 39.9) setter('Obesity Level II', BMI, '#ff6767');
-        else if (BMI >= 40) setter('Obesity Level III', BMI, 'red');
+        else if (BMI > 30 && BMI <= 34.9) setter('Obesity Level-I', BMI, "#f99301");
+        else if (BMI > 35 && BMI <= 39.9) setter('Obesity Level-II', BMI, '#ff6767');
+        else if (BMI >= 40) setter('Obesity Level-III', BMI, 'red');
 
         // clean up code
         height.value = weight.value = '';
